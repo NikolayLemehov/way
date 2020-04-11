cce41c601f6a246305b34164096ffe6fbb32ba50
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-04-11 21:48:19 +0300

    Рефакторит main.js для удобства восприятия

--- source/js/main.js
+++ source/js/main.js
@@ -4,13 +4,14 @@ import Country from './country';
 import {activeForEeachNodeListForIE} from './utils';
 
 activeForEeachNodeListForIE();
+
+const moveTo = new window.MoveTo();
+const triggerCollection = document.querySelectorAll(`.js-trigger`);
+triggerCollection.forEach((it) => moveTo.registerTrigger(it));
+
 const pageHeader = new PageHeader(document.querySelector(`.page-header`));
 const visitPlace = new VisitPlace(document.querySelector(`.visit-place`));
 const country = new Country(document.querySelector(`.country`));
 pageHeader.active();
 visitPlace.active(country);
 country.active();
-
-const moveTo = new window.MoveTo();
-const triggerCollection = document.querySelectorAll(`.js-trigger`);
-triggerCollection.forEach((it) => moveTo.registerTrigger(it));
